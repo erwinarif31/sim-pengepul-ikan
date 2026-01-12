cea00f430b24481a575ec94d3e249fad5aca9ae5
Muhammad Erwin Arif <muherwinarif31@gmail.com>
2026-01-12 09:22:26 +0800

    chore(client): update docs and breadcrumb component

--- client/src/component/common/PageBreadCrumb.tsx
+++ client/src/component/common/PageBreadCrumb.tsx
@@ -2,17 +2,21 @@ import { Link } from "react-router-dom";
 
 interface BreadcrumbProps {
   pageTitle: string;
+  children?: React.ReactNode;
 }
 
-const PageBreadcrumb: React.FC<BreadcrumbProps> = ({ pageTitle }) => {
+const PageBreadcrumb: React.FC<BreadcrumbProps> = ({ pageTitle, children }) => {
   return (
     <div className="flex flex-wrap items-center justify-between gap-3 mb-6">
-      <h2
-        className="text-xl font-semibold text-gray-800 dark:text-white/90"
-        x-text="pageName"
-      >
-        {pageTitle}
-      </h2>
+      <div className="flex items-center gap-3">
+        <h2
+          className="text-xl font-semibold text-gray-800 dark:text-white/90"
+          x-text="pageName"
+        >
+          {pageTitle}
+        </h2>
+        {children}
+      </div>
       <nav>
         <ol className="flex items-center gap-1.5">
           <li>
